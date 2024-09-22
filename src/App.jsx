8651684c505ad3ef21485bad7d1b575bb31f6b42
codic-yeeshu <yeeshu.exploresmore@gmail.com>
2024-09-22 22:51:27 +0530

    initial ui updated

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SideBar from "./components/sidebar/sidebar";
+import Main from "./components/main/main";
+
+const App = () => {
+  return (
+    <>
+      <SideBar />
+      <Main />
+    </>
+  );
+};
+
+export default App;
