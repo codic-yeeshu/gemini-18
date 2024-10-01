9bf70db32ae4506e281e897b51015e579d194859
codic-yeeshu <yeeshu.exploresmore@gmail.com>
2024-10-01 17:48:03 +0530

    updated user dp

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -67,7 +67,7 @@ const Main = () => {
           ) : (
             <div className="result">
               <div className="result-title">
-                <img src={assets.user_icon} alt="" />
+                <img src={assets.blank_dp} alt="" />
                 <p>{recentPrompt}</p>
               </div>
               <div className="result-data">
